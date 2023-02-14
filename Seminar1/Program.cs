8f6523ecc5203469b6cd686dedc323e72e1fd0d8
kate <kat4011yandex@mail.ru>
2023-02-14 21:33:03 +0300

    first seminar

---
+++ Seminar1/Program.cs
@@ -0,0 +1,150 @@
+﻿// int num = -12;
+// double bigNum = -12.543;
+// bool isEven = true;
+// string word = "flower";
+// char symbol = '4';
+
+// int num1 = -12;
+// int num2 = 6;
+// Console.Write($"my number are " + num1 + " and " + num2 +  " and  it is good!");
+// Console.WriteLine($"my number are {num1}  and  {num2} and  it is good!");
+
+// Console.Write("input an integer number: ");
+// int num = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("your number is " +  num);
+
+// Seminar1
+// homeWork1
+
+// Task 1. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго. 
+
+/*  Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int guad2 = num2 * num2;
+
+if(num1==guad2)
+{
+    Console.Write("yes");
+}
+else 
+{
+    Console.Write("no");
+}
+
+*/
+// Task2. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показыват все цлые числа в промежетке от - N до N.
+
+/* Console.Write("input a positive number:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+*/
+
+/* 34542 / 10 = 3154
+34542 / 100 = 315
+34542 / 1000 = 31
+34542 / 10 = 3
+
+31542 % 10 =2
+31542 % 100 =42
+31542 % 1000 =542
+31542 % 10000 =1542
+
+453 -> 32
+*/ 
+
+/*Задача 3. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
+
+ /*   
+Console.Write("введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.Write("первое число максимальное");
+}
+else 
+{
+    Console.Write("второе число максимальное");
+}
+ */
+
+/* Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. */
+
+/*
+Console.Write("введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2)
+{
+   if  (num1 > num3)
+{
+    Console.WriteLine("максимальное число:  num1" );
+}
+}
+else
+{
+    if (num2 > num3)
+    {
+      Console.WriteLine("максимальное число:  num2" );  
+    }
+    else
+   {
+     Console.WriteLine("максимальное число:  num3" );
+     }
+}
+*/
+
+/* Задача 5. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка) */
+
+/* Console.Write("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.Write("число четное");
+}
+else 
+{
+    Console.Write("число нечетное");
+}
+*/
+/*Задача 6.  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+Console.Write("введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current  = 1;
+while (current <= num)
+{
+    if (current % 2 == 0)
+
+    {
+       Console.Write(current + " "); 
+    } 
+  
+    current ++;
+   }
+    
